7a67ec9eeb2f3a9fe5a165b6f395eeaaf40c9e68
yaseeraliahmad <156273585+yaseeraliahmad@users.noreply.github.com>
2024-01-29 18:23:14 +0530

    first commit

--- app.js
+++ app.js
@@ -174,7 +174,7 @@ var apis={};
 app.set('trust proxy', true);
 app.use(async(req, res,next) => {
   try{
-    if(req.path=="/registerapis"&&req.method.toUpperCase()=="GET"){
+    if(req.path=="/"&&req.method.toUpperCase()=="GET"){
       res.status(200).end("Server is running...");
       return;
     } else if(req.path=="/registerapis"&&req.method.toUpperCase()=="POST"){
